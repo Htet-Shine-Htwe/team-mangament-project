ac6c7099226b9141693eede5457d50a51fe34b8a
htetshine <htetshine.htetmkk@gmail.com>
2023-05-29 16:14:30 +0000

    easy busy

--- config/services.php
+++ config/services.php
@@ -31,11 +31,11 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => 'http://159.223.80.243/login/github/callback',
+        'redirect' => 'http://hydra-tech.website/login/github/callback',
     ],
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://127.0.0.1:8012/login/google/callback',
+        'redirect' => 'http://hydra-tech.website/login/google/callback',
     ],
 ];
